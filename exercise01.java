e4f6c550c041a43ffbdc6c7d1f8bdb478ce0a3a4
marian4cost <mariana.costa.lisboa5579@gmail.com>
2026-01-19 13:03:22 -0300

    exercise 01

---
+++ exercise01.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class exercise01 {
+    public void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("qual seu nome: ");
+        String name = scanner.nextLine();
+
+        System.out.println("oiee " + name);
+    }
+}
+
