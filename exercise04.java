bcf63105b70914fb60d528d5964d792206e4abd3
marian4cost <mariana.costa.lisboa5579@gmail.com>
2026-02-18 18:56:29 -0300

    exercise 04

---
+++ exercise04.java
@@ -0,0 +1,13 @@
+public class exercise04 {
+    public void main(String[] args){
+        int number = 1;
+        double mean = 5.5;
+        char Char = 'A';
+        String text = "mariana";
+
+        System.out.println(number);
+        System.out.println(mean);
+        System.out.println(Char);
+        System.out.println(text);
+    }
+}
