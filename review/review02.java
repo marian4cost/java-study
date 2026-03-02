3a84a4b99d0a789d2361cef309104a4e18a61b17
marian4cost <mariana.costa.lisboa5579@gmail.com>
2026-03-02 20:42:03 -0300

    sacnner review

---
+++ review/review02.java
@@ -0,0 +1,13 @@
+package review;
+
+import java.util.Scanner;
+
+public class review02 {
+    public void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+
+        String nome = scanner.nextLine();
+
+        System.out.println(nome);
+    }
+}
