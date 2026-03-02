e775bda4ac17b17125e16b4f96c84fd976c0196e
marian4cost <mariana.costa.lisboa5579@gmail.com>
2026-03-02 20:39:04 -0300

    variables review

---
+++ review/review01.java
@@ -0,0 +1,44 @@
+package review;
+
+public class review01 {
+    public void main (String[] args){
+        // inteiros, decimais, booleaos, texto
+
+        //INTEIROS
+        byte var1;
+        var1 = 75;
+
+        short var2 = 32000;
+
+        int var3 = 20000000;
+
+        long var4 = 80000000000000l;
+
+        System.out.println(var1);
+        System.out.println(var2);
+        System.out.println(var3);
+        System.out.println(var4);
+
+        //REAIS
+        float var5 = 55.2f;
+        double var6 = 3.14020202020202220202020505;
+
+        System.out.println(var5);
+        System.out.println(var6);
+
+        //CARACTER
+        char var7 = 'g';
+
+        System.out.println(var7);
+
+        //BOOLEANO
+        boolean var8 = true;
+
+        System.out.println(var8);
+
+        //STRING - não é bem um tipo primitivo é um classe (objeto)
+        String var9 = "genilson";
+
+        System.out.println(var9);
+    }
+}
