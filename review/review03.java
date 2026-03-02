516f7a837449b7f50388b54d9f76a4d402170e72
marian4cost <mariana.costa.lisboa5579@gmail.com>
2026-03-02 20:55:55 -0300

    random review

---
+++ review/review03.java
@@ -0,0 +1,13 @@
+package review;
+
+import java.util.Random;
+
+public class review03 {
+    public void main(String[] args){
+        Random random = new Random();
+
+        int sorteio = random.nextInt(20);
+
+        System.out.println(sorteio);
+    }
+}
