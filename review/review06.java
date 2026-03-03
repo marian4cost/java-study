5b1a93de4c83bff8b65c81518f10e9ddd55b6e6a
marian4cost <mariana.costa.lisboa5579@gmail.com>
2026-03-03 13:43:52 -0300

    structure repeticion review --while

---
+++ review/review06.java
@@ -0,0 +1,12 @@
+package review;
+
+public class review06 {
+    public void main(String[] args){
+        int contador = 0;
+
+        while(contador <= 5){
+            System.out.println(contador);
+            contador = contador + 1;
+        }
+    }
+}
