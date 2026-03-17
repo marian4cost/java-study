6975d14c893bb779f0988b4c38da027fc24365fd
marian4cost <mariana.costa.lisboa5579@gmail.com>
2026-03-17 13:13:33 -0300

    exercise01 --class

---
+++ poo/exercise/exercise01.java
@@ -0,0 +1,21 @@
+package poo.exercise;
+
+class Pessoa{
+    String name;
+    int age;
+}
+
+public class exercise01 {
+    public void main(String[] args){
+        Pessoa p1 = new Pessoa();
+        p1.name = "genisson";
+        p1.age = 19;
+
+        Pessoa p2 = new Pessoa();
+        p2.name = "mariana";
+        p2.age = 19;
+
+        System.out.println("nome " + p1.name + " idade " + p1.age);
+        System.out.println("nome " + p2.name + " idade " + p2.age);
+    }
+}
